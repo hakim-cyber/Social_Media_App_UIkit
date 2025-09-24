acdde68587879151da5e803b3f6e85520c064f93
Hakim Aliyev <115820565+hakim-cyber@users.noreply.github.com>
2025-09-25 03:16:18 +0400

    changed tint color of app

--- Social_Media_App_UIkit/Support/SceneDelegate.swift
+++ Social_Media_App_UIkit/Support/SceneDelegate.swift
@@ -19,6 +19,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         let window = UIWindow(windowScene: windowScene)
+        window.tintColor = UIColor.electricPurple // your custom color
         window.rootViewController = UINavigationController(rootViewController: LoginViewController())
         window.makeKeyAndVisible()
         
